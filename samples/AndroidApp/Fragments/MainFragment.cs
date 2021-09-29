e936952286157a9b074c8e4bc7a3206c2e0950f3
Panos <panos_ath@outlook.com>
2021-09-29 16:01:49 +0300

    Update Sample
    Update sample with one type in a nested namespace to test if the generation works!

---
+++ samples/AndroidApp/Fragments/MainFragment.cs
@@ -0,0 +1,21 @@
+﻿using Android.OS;
+using Android.Views;
+using P41.ViewBindingsGenerator;
+using Fragment = AndroidX.Fragment.App.Fragment;
+
+namespace AndroidApp.Fragments;
+
+[GenerateBindings("fragment_main.xml")]
+public partial class MainFragment : Fragment
+{
+    public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        var view = inflater.Inflate(Resource.Layout.fragment_main, container, false)!;
+
+        HelloText.Text = "Hello Generetor";
+        ClickMe.Text = "Generated";
+        Progress.Indeterminate = true;
+
+        return view;
+    }
+}
--- samples/AndroidApp/MainActivity.cs
+++ samples/AndroidApp/MainActivity.cs
@@ -1,9 +1,7 @@
 using Android.App;
 using Android.OS;
-using Android.Views;
 using AndroidX.AppCompat.App;
 using P41.ViewBindingsGenerator;
-using Fragment = AndroidX.Fragment.App.Fragment;
 
 namespace AndroidApp;
 
@@ -19,20 +17,5 @@ public partial class MainActivity : AppCompatActivity
         HelloText.Text = "Hello Generetor";
         ClickMe.Text = "Generated";
         Progress.Indeterminate = true;
-    }
-}
-
-[GenerateBindings("fragment_main.xml")]
-public partial class MainFragment : Fragment
-{
-    public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-    {
-        var view = inflater.Inflate(Resource.Layout.fragment_main, container, false)!;
-
-        HelloText.Text = "Hello Generetor";
-        ClickMe.Text = "Generated";
-        Progress.Indeterminate = true;
-
-        return view;
     }
 }
